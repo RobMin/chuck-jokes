32a37e56d17f92c4bd5cf605e0b7876401683a86
RobMin <robertminasyan01@gmail.com>
2020-11-19 15:44:29 +0400

    initial commit

---
+++ PLAN.md
@@ -0,0 +1,5 @@
+Read the task description once more
+Create initial cra + ts template and clean it
+Add scss
+Do the initial commit
+
---
+++ src/App/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.scss';
+
+const App = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default App;
